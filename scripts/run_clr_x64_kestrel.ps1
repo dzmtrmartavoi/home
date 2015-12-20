14bf0606a9a85291bcc4087a96f7b1203e98ca68
Dzmitry Martavoi <Dzmitry Martavoi>
2015-12-21 02:16:49 +0300

    change kestrel configuration

--- scripts/run_clr_x64_kestrel.ps1
+++ scripts/run_clr_x64_kestrel.ps1
@@ -1,3 +1,3 @@
 dnvm install latest -r clr -arch x64
 $proj = Join-Path $PSScriptRoot ..\src\home\project.json
-Start-Process dnx -ArgumentList --project, "`"$proj`"", kestrel -Verb runas 
+Start-Process dnx -ArgumentList --project, "`"$proj`"", kestrel -Verb runas
